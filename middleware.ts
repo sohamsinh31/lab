450c51c832317919bd044b18c02557848d340a9b
alok_mishra14 <143alokmishra143@gmail.com>
2024-12-22 16:45:26 +0530

    FIX middleware

--- middleware.ts
+++ middleware.ts
@@ -4,18 +4,14 @@ import { getSession } from "./components/action/getSession";
 export default async function middleware(req: NextRequest) {
   const session = await getSession(req);
   const { pathname } = req.nextUrl;
-
   const protectedPaths = ["/", "/dashboard"]; // Add paths to protect
   const loginPath = "/auth/login";
-
   // Only run middleware for protected paths
   if (!protectedPaths.includes(pathname)) {
     return NextResponse.next();
   }
-
   if (!session) {
     return NextResponse.redirect(new URL(loginPath, req.nextUrl.origin));
   }
-
   return NextResponse.next();
 }
